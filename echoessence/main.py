95fbbb6cb7b7353116c0a419ec1b199e8b1ab851
Sameer Khan A <153911733+sameerkhan2003a@users.noreply.github.com>
2024-08-08 13:38:37 +0530

    Update main.py

--- echoessence/main.py
+++ echoessence/main.py
@@ -21,7 +21,7 @@ def index():
                 file=audio_file
             )
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
+            model="gpt-4o-mini",
             temperature=0,
             max_tokens=256,
             messages=[
